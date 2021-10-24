6042053a9efbfa5fd2cb42a81a5398610430ad2b
jarvisye <972763217@qq.com>
2021-10-24 12:06:02 +0800

    20211024: Add condition, countlatch and barrier.

--- mutex/jmutex.cpp
+++ mutex/jmutex.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include "jmutex.hpp"
 #include "util/jtime.hpp"
 
@@ -37,6 +38,112 @@ int JMutex::TryLock(int mc)
             return 0;
     }
     return -1;
+}
+
+pthread_mutex_t& JMutex::GetMutex()
+{
+    return mutex;
+}
+
+JMutexHelper::JMutexHelper(JMutex & m): mutex(m)
+{
+    assert(mutex.Lock() == 0);
+}
+
+JMutexHelper::~JMutexHelper()
+{
+    assert(mutex.UnLock() == 0);
+}
+
+JCond::JCond(JMutex & m): mutex(m)
+{
+    pthread_cond_init(&cond, NULL);
+}
+
+int JCond::Lock()
+{
+    return mutex.Lock();
+}
+
+int JCond::Wait()
+{
+    if (pthread_cond_wait(&cond, &mutex.GetMutex()) == 0)
+        return 0;
+    return -1;
+}
+
+int JCond::Signal()
+{
+    if (pthread_cond_signal(&cond) == 0)
+        return 0;
+    return -1;
+}
+
+int JCond::Broadcast()
+{
+    if (pthread_cond_broadcast(&cond) == 0)
+        return 0;
+    return -1;
+}
+
+JCountLatch::JCountLatch(int c, JMutex & m):
+count(c), mutex(m), cond(mutex)
+{}
+
+JCountLatch::~JCountLatch() {}
+
+bool JCountLatch::HasDone()
+{
+    bool ret;
+    {
+        JMutexHelper h(mutex);
+        ret = HasDoneNonLock();
+    }
+    return ret;
+}
+
+bool JCountLatch::HasDoneNonLock()
+{
+    return count == 0;
+}
+
+int JCountLatch::Done()
+{
+    {
+        JMutexHelper h(mutex);
+        if (--count == 0)
+            cond.Signal();
+    }
+    return 0;
+}
+
+int JCountLatch::Wait()
+{
+    cond.Lock();
+    while (!HasDone())
+        cond.Wait();
+    return 0;
+}
+
+JBarrier::JBarrier(int c, JMutex & m):
+count(c), mutex(m), cond(mutex)
+{}
+
+JBarrier::~JBarrier() {}
+
+int JBarrier::Wait()
+{
+    JMutexHelper h(mutex);
+    if (--count == 0)
+    {
+        cond.Broadcast();
+    }
+    else
+    {
+        while (count > 0)
+            cond.Wait();
+    }
+    return 0;
 }
 
 }   // namespace jarvis
--- mutex/jmutex.hpp
+++ mutex/jmutex.hpp
@@ -16,8 +16,69 @@ public:
     int UnLock();
     // try lock(mc millseconds)
     int TryLock(int mc);
+
+    pthread_mutex_t& GetMutex();
 private:
     pthread_mutex_t mutex;
+};
+
+class JMutexHelper
+{
+public:
+    JMutexHelper(JMutex & m);
+    ~JMutexHelper();
+private:
+    JMutex & mutex;
+};
+
+class JCond
+{
+public:
+    JCond(JMutex & m);
+    ~JCond();
+
+    // lock before wait
+    int Lock();
+    // 0: success -1: fail
+    int Wait();
+    // signal
+    int Signal();
+    // broadcast
+    int Broadcast();
+private:
+    JMutex & mutex;
+    pthread_cond_t cond;
+};
+
+class JCountLatch
+{
+public:
+    JCountLatch(int c, JMutex & m);
+    ~JCountLatch();
+
+    int Done();
+    int Wait();
+    bool HasDone();
+private:
+    // before wait you should check hasdone
+    bool HasDoneNonLock();
+    volatile int count;
+    JMutex & mutex;
+    JCond cond;
+
+};
+
+class JBarrier
+{
+public:
+    JBarrier(int c, JMutex & m);
+    ~JBarrier();
+    // wait for other threads
+    int Wait();
+private:
+    volatile int count;
+    JMutex & mutex;
+    JCond cond;
 };
 
 }   // namespace jarvis
